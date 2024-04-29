37d2d6ec293cf9f7ef4083c479c88ae4e1e4e513
Maxie Cletus <maxiecletus@gmail.com>
2024-04-29 21:48:13 +1000

    new changes

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
-const path = require('path')
+// server.js
+
 const express = require('express');
-const connectDB = require('./dbconnection');
+const http = require('http');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const routes = require('./routes/index');
+const socketIo = require('socket.io');
+const path = require('path');
+const connectDB = require('./dbconnection');
+const cardRouter = require('./routes/index'); // Assuming routes are defined in index.js
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.set('socketio', io); // Attach io instance to app
+
+// Connect to the database
+connectDB();
+
+// Express middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Connect to MongoDB
-connectDB();
+// Setup routes
+app.use('/', cardRouter);
 
-app.use('/', routes); // Use routes defined in index.js
+// Socket.io connection
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+
+    socket.on('createCard', (card) => {
+        io.emit('cardCreated', card); // Emit to all clients, including sender
+    });
+});
+
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
